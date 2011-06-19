17b0c7c9d493003c26c5ca8ffe134b25dfb4d84a
Raptrex <raptrex@gmail.com>
2011-06-18 23:24:29 -0700

    getting 500 error

--- xmlparser.php
+++ xmlparser.php
@@ -1,12 +1,12 @@
 <?php 
 
-class reddit
+class Reddit
 {
 	public $title = '';
 	public $link = '';
 	public $date = '';
 	public $description = '';
-	
+
 	function __construct($title, $link, $date, $description) 
 	{
 		$this->title = $title;
@@ -19,13 +19,67 @@ class reddit
 $request_url = "http://www.reddit.com/.xml"; 
 $xml = simplexml_load_file($request_url) or die("feed not loading");
 
+$thisPage = [];
+
 foreach($xml->channel->item as $item)
 {
+	/*
 	echo "<div class=\"item\">";
 	echo "<a href=" .$item->link. ">" .$item->title. "</a>";
 	echo "<p>" .$item->pubDate. "</p>";
 	echo "<p>" .$item->description. "</p>";
 	echo "</div>";
+	*/
+
+	$reddit =& new Reddit($item->title, $item->link, 
+								$item->pubDate, $item->description);
+
+	array_push($thisPage, $reddit);
 }
 
 ?>
+
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+	<title>Reddit Tablet</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+	<div id="navigation">
+		<ul>
+			<li><a href="#">Front page</a></li>
+			<li><a href="#">Messages</a></li>
+			<h3>Subscriptions</h3>
+			<li class="active"><a href="#">Ask Reddit</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Funny</a></li>
+			<li><a href="#">Gaming</a></li>
+			<li><a href="#">Pics</a></li>
+			<li><a href="#">Programming</a></li>
+			<li><a href="#">Starcraft</a></li>
+			<li><a href="#">WTF</a></li>
+			<li><a href="#">Ask Reddit</a></li>
+			<li><a href="#">Blog</a></li>
+			<li><a href="#">Funny</a></li>
+			<li><a href="#">Gaming</a></li>
+			<li><a href="#">Pics</a></li>
+			<li><a href="#">Programming</a></li>
+			<li><a href="#">Starcraft</a></li>
+			<li><a href="#">WTF</a></li>
+		</ul>
+	</div>
+
+	<div id="posts">
+		<?php
+			foreach($thisPage as $i)
+			{
+				echo "<section>" .$thisPage[$i]->title;
+				echo "</section>";
+			}
+		?>
+	</div>
+
+</body>
+</html>
